0fa74116276bb2e4259e3c2541260d56126d10b5
Adory Vo <business@adoryvo.com>
2023-02-22 22:32:50 -0800

    Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,9 +2,9 @@
 	<main>
 		<BodyTitle>about</BodyTitle>
 		<p>
-			Hey! I'm <b class="text-blue-300">Adory</b>, a senior at <b class="text-blue">UC San Diego</b> studying
-			math &amp; computer science
-			to pursue a career in software development and potentially teaching.
+			Hey! I'm <b class="text-blue-300">Adory</b>, an alum with a B.S. in
+			Math &amp; Computer Science from <b class="text-blue">UC San Diego</b>
+			pursuing a career in software development and potentially teaching.
 			I have been doing freelance web development for about three years now and this website is a web development
 			project of mine
 			where I'll be showcasing some of my work.
@@ -21,8 +21,7 @@
 			<li>I enjoy tutoring/teaching computer science and math (tutored for CSE 12/15L under Gary Gillespie)</li>
 			<li>Favorite color: navy blue</li>
 			<li>Favorite food: pasta</li>
-			<li>Favorite television shows: Sherlock, Jujutsu Kaisen, Parks and Recreation</li>
-			<li>Favorite video game franchises: Pokémon, Smash Bros, League of Legends, Overcooked</li>
+			<li>Favorite video game franchises: Pokémon, Nintendo games, League of Legends</li>
 			<li><i><NuxtLink class="link-primary" to="/music">Click here to judge my music taste</NuxtLink></i>
 			</li>
 		</ul>
