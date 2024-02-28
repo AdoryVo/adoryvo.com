f335e4528ba5d6d49d53ca457f0643a90f328c9c
Adory Vo <business@adoryvo.com>
2024-02-28 00:49:06 -0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/static'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     server: {
       watch: {
-        usePolling: true, // Needed for refresh in WSL, see Astro issue #6043
-      },
-    },
+        usePolling: true // Needed for refresh in WSL, see Astro issue #6043
+      }
+    }
   },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
-  site: 'https://www.adoryvo.com',
-})
+  site: 'https://www.adoryvo.com'
+});
